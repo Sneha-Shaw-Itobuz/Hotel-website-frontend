e2ec347ca0ca84764096253b7f2ac30c374d59a8
Sneha-Shaw-Itobuz <sneha@itobuz.com>
2023-03-27 16:30:58 +0530

    index changes

--- js/script.js
+++ js/script.js
@@ -1,19 +1,13 @@
-const banner = document.querySelector("#banner");
-
-let bannerText = `<h1 class="text-6xl text-white mb-10 font-light">Enjoy your <br> dream vacation</h1>
-<p class="text-white">Quite and endlessly beautiful hideaway</p>
-<div class="mt-5"> <button class="bg-white p-4 text-black">About Us </button>
- <button class="text-white ml-7">View Homestay</button></div>
-`;
-
-(function addContent() {
-  // banner.style.background= "url(.)"
-  document.querySelector(".left-text").innerHTML = bannerText;
-})();
+const menubar = document.querySelector(".menu");
+const list = document.querySelector(".list");
 
 var swiper = new Swiper(".mySwiper", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+menubar.addEventListener("click", () => {
+  list.classList.toggle("hidden");
+});
